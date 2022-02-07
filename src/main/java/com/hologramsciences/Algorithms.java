22838279fefba6b3e314526e6e9a15bf72fcbac2
Nathaniel Lim <nathaniel.j.lim@gmail.com>
2022-02-07 09:48:13 -0600

    Save progress

--- src/main/java/com/hologramsciences/Algorithms.java
+++ src/main/java/com/hologramsciences/Algorithms.java
@@ -1,4 +1,52 @@
 package com.hologramsciences;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Algorithms {
+    /**
+     *
+     *  Compute the cartesian product of a list of lists of any type T
+     *  the result is a list of lists of type T, where each element comes
+     *  each successive element of the each list.
+     *
+     *  https://en.wikipedia.org/wiki/Cartesian_product
+     *
+     *  For this problem order matters.
+     *
+     *  Example:
+     *
+     *   listOfLists = Arrays.asList(
+     *                         Arrays.asList("A", "B"),
+     *                         Arrays.asList("K", "L")
+     *                 )
+     *
+     *   returns:
+     *
+     *   Arrays.asList(
+     *         Arrays.asList("A", "K"),
+     *         Arrays.asList("A", "L"),
+     *         Arrays.asList("B", "K"),
+     *         Arrays.asList("B", "L")
+     *   )
+     *
+     *
+     *
+     */
+    public static final <T> List<List<T>> cartesianProductForLists(final List<List<T>> listOfLists) {
+        // TODO Implement me
+        return Collections.emptyList();
+    }
+
+    /**
+     *
+     *  In the United States there are six coins:
+     *  1¢ 5¢ 10¢ 25¢ 50¢ 100¢
+     *  Assuming you have an unlimited supply of each coin,
+     *  implement a method which returns the number of distinct ways to make totalCents
+     */
+    public static final long countNumWaysMakeChange(final int totalCents) {
+        // TODO Implement me
+        return 0L;
+    }
 }
--- src/test/java/com/hologramsciences/AlgorithmsTest.java
+++ src/test/java/com/hologramsciences/AlgorithmsTest.java
@@ -1,14 +1,74 @@
 package com.hologramsciences;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 public class AlgorithmsTest {
 
     @Test
-    public void testWaysToMakeChange() {
-        assertTrue(true);
+    public void testCartesianProduct1() {
+        final List<List<String>> listOfLists = Arrays.asList(
+                Arrays.asList("A", "B"),
+                Arrays.asList("K", "L")
+        );
+
+        assertEquals(
+                Algorithms.cartesianProductForLists(listOfLists),
+
+                Arrays.asList(
+                        Arrays.asList("A", "K"),
+                        Arrays.asList("A", "L"),
+                        Arrays.asList("B", "K"),
+                        Arrays.asList("B", "L")
+                )
+        );
+    }
+
+    @Test
+    public void testCartesianProduct2() {
+        final List<List<Integer>> listOfLists = Arrays.asList(
+                Arrays.asList(1, 2),
+                Arrays.asList(3, 4),
+                Arrays.asList(5, 6, 7)
+        );
+
+        assertEquals(
+                Algorithms.cartesianProductForLists(listOfLists),
+
+                Arrays.asList(
+                        Arrays.asList(1, 3, 5),
+                        Arrays.asList(1, 3, 6),
+                        Arrays.asList(1, 3, 7),
+
+                        Arrays.asList(1, 4, 5),
+                        Arrays.asList(1, 4, 6),
+                        Arrays.asList(1, 4, 7),
+
+                        Arrays.asList(2, 3, 5),
+                        Arrays.asList(2, 3, 6),
+                        Arrays.asList(2, 3, 7),
+
+                        Arrays.asList(2, 4, 5),
+                        Arrays.asList(2, 4, 6),
+                        Arrays.asList(2, 4, 7)
+                )
+        );
+    }
+
+
+    @Test
+    public void testWaysToMakeChange200Cents() {
+        assertTrue(Algorithms.countNumWaysMakeChange(200) == 2728L);
+    }
+
+    @Test
+    public void testWaysToMakeChange20000Cents() {
+        assertTrue(Algorithms.countNumWaysMakeChange(20000) == 4371565890901L);
     }
 }
 
